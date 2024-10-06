348dfd563075d90588f6df62ae915dcd64a9373b
Isaiah Odhner <isaiahodhner@gmail.com>
2024-10-06 13:29:43 -0400

    Create helpers.js

--- game.js
+++ game.js
@@ -3,13 +3,6 @@
 const suits = ['♠', '♥', '♦', '♣'];
 const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-function shuffleArray(array) {
-	for (let i = array.length - 1; i >= 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
-	}
-}
-
 function dealCards() {
 	const deck = [];
 	for (const suit of suits) {
